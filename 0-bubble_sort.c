44d93571180e9955e715b8e0d1b7687c7f119370
= <=>
2023-07-24 16:38:23 +0300

    Optimize bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,19 +14,26 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
+	int temp, swap;
+
+	if (array == NULL || size == 0)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
+		swap = 0;
 		for (j = 1; j < size; j++)
 		{
 			if (array[j - 1] > array[j])
 			{
+				swap = 1;
 				temp = array[j - 1];
 				array[j - 1] = array[j];
 				array[j] = temp;
 				print_array(array, size);
 			}
 		}
+		if (swap == 0)
+			break;
 	}
 }
