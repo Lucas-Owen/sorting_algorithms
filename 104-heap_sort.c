fb6be49a53b195d6ff8168ce33cd5364d0bee0f1
= <=>
2023-07-26 19:18:03 +0300

    Optimize heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -15,36 +15,69 @@ void swapInt(int *a, int *b)
 }
 
 /**
+ * heapify - Maintain array as heap (as binary tree)
+ * @array: The array
+ * @start: start index
+ * @size: Size of the array
+ * @actualSize: Size of the array
+ */
+void heapify(int *array, long start, long size, long actualSize)
+{
+	int leftChild, rightChild, i;
+
+	leftChild = 2 * start + 1;
+	rightChild = 2 * start + 2;
+	if (leftChild >= size)
+		return;
+	i = leftChild;
+	if (rightChild < size)
+		i = (array[leftChild] < array[rightChild] ? rightChild : leftChild);
+	if (array[start] < array[i])
+	{
+		swapInt(&array[i], &array[start]);
+		print_array(array, actualSize);
+		heapify(array, i, size, actualSize);
+	}
+	return;
+}
+
+/**
  * sift_down - Brings the biggest element in the array to the top
  * (the array is treated as a binary tree)
  *
  * @array: The array
- * @start: Start index of creating the max heap
- * @size: Size of the unsorted portion of the array
- * @actualSize: Size of the array (for printing)
- * Return: int - index of greater sibling, -1 when past the array size
+ * @size: Size of the unsorted part of the array
+ * @actualSize: Size of the array
+ * Return: void
  */
-int sift_down(int *array, int start, long size, size_t actualSize)
+void sift_down(int *array, long size, long actualSize)
 {
-	int i, j, k;
+	int parent, k;
+	int leftChild = -1, rightChild = -1;
+	int start = size - 1;
 
-	if (start >= size)
-		return (-1);
-	i = sift_down(array, 2 * start + 1, size, actualSize);
-	j = sift_down(array, 2 * start + 2, size, actualSize);
-	if (i == -1)
-		return (start);
-	else if (j == -1)
-		k = i;
+	if (start % 2)
+		leftChild = start;
 	else
-		k = (array[i] > array[j] ? i : j);
-	if (array[start] < array[k])
+		rightChild = start;
+	if (leftChild == -1)
+		leftChild = rightChild - 1;
+	else
+		rightChild = leftChild + 1;
+	for (; leftChild > 0; leftChild -= 2, rightChild -=2)
 	{
-		swapInt(&array[k], &array[start]);
-		print_array(array, actualSize);
-		sift_down(array, start, size, actualSize);
+		if (rightChild < size)
+			k = (array[leftChild] > array[rightChild] ? leftChild : rightChild);
+		else
+			k = leftChild;
+		parent = (leftChild - 1) / 2;
+		if (array[parent] < array[k])
+		{
+			swapInt(&array[k], &array[parent]);
+			print_array(array, actualSize);
+			heapify(array, k, size, actualSize);
+		}
 	}
-	return (start);
 }
 
 /**
@@ -66,7 +99,7 @@ void heap_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		sift_down(array, 0, size - i, size);
+		sift_down(array, size - i, size);
 		swapInt(&array[0], &array[size - i - 1]);
 		print_array(array, size);
 	}
